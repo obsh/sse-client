0f97a8e0ec15ac27adb8e811b62c41bfc02c3790
oleksandr.bushkovskyi <bushkovsky@gmail.com>
2015-10-19 14:52:20 +0300

    Implement lastId and retry period handling.

--- src/Client.php
+++ src/Client.php
@@ -9,8 +9,13 @@ class Client
     private $client;
     /** @var GuzzleHttp\Psr7\Response */
     private $response;
-    /** @var string */
+
+    /** @var string - path to listen, must start from root "/" */
     private $path;
+    /** @var  string - last received message id */
+    private $lastId;
+    /** @var  int - reconnection time in milliseconds */
+    private $retry = 3000;
 
     public function __construct($baseUri, $path = '/')
     {
@@ -21,12 +26,18 @@ class Client
 
     private function connect()
     {
+        $headers = [
+            'Accept' => 'text/event-stream',
+            'Cache-Control' => 'no-cache'
+        ];
+
+        if ($this->lastId) {
+            $headers['Last-Event-ID'] = $this->lastId;
+        }
+
         $this->response = $this->client->request('GET', sprintf('%s.json', $this->path), [
             'stream' => true,
-            'headers' => [
-                'Accept' => 'text/event-stream',
-                'Cache-Control' => 'no-cache'
-            ]
+            'headers' => $headers
         ]);
     }
 
@@ -50,6 +61,17 @@ class Client
                 $buffer = $remaining;
 
                 $event = Event::parse($rawMessage);
+
+                // if message contains id set it to last received message id
+                if ($event->getId()) {
+                    $this->lastId = $event->getId();
+                }
+
+                // take into account server request for reconnection delay
+                if ($event->getRetry()) {
+                    $this->retry = $event->getRetry();
+                }
+
                 yield $event;
             }
         }
